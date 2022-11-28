7af5e6758745d77bedaf22db2192c2c7df5ee404
your_github_username <your_github_email>
2022-11-28 18:13:45 +0700

    mvvm

---
+++ MvvmProject/Data/Model/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  MvvmProject
+//
+//  Created by Test VPN on 28/11/2022.
+//
+
+import Foundation
+
+struct User: Codable{
+    var login: String?
+    var avatar_url: String?
+    var html_url: String?
+}
---
+++ MvvmProject/Data/Model/UserDetail.swift
@@ -0,0 +1,10 @@
+import Foundation
+struct UserDetail: Codable{
+    let name: String
+    let avatar_url: String
+    let bio: String
+    let followers: Int64
+    let email: String
+    let created_at: Date
+    let login: String
+}
